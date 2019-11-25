ca4a72fbc8df664bd0a21218c7cb1089111e97ef
nana <nykim@nykim.net>
2019-11-25 19:02:09 +0900

    1차 작업 완료

--- js/ui.js
+++ js/ui.js
@@ -1,23 +1,58 @@
-var hyoduSVG = new Vivus("hyoduSVG", {
-  type: 'scenario-sync',
-  duration: 20,
-  start: 'manual',
-  forceRender: false,
-  dashGap: 20
-});
+(function () {
+  var hyoduSVG = new Vivus("hyoduSVG", {
+    type: 'scenario-sync',
+    duration: 19,
+    start: 'manual',
+    forceRender: false,
+    dashGap: 20
+  });
 
-var hyoduSVG = new Vivus("lines", {
-  type: 'async',
-  duration: 50,
-  start: 'autostart',
-  forceRender: false,
-});
+  var lineSVG01 = new Vivus("lines", {
+    type: 'async',
+    duration: 50,
+    start: 'autostart',
+    forceRender: false,
+  });
 
 
-var hyoduSVG = new Vivus("lines2", {
-  type: 'async',
-  duration: 80,
-  start: 'autostart',
-  forceRender: false,
-  dashGap: 50
-});
+  var lineSVG02 = new Vivus("lines2", {
+    type: 'async',
+    duration: 80,
+    start: 'autostart',
+    forceRender: false,
+    dashGap: 50
+  });
+
+
+  var drawHyodu = function () {
+    hyoduSVG.reset().play();
+  }
+
+  var textEl = document.querySelector(".aniText");
+  var footerEl = document.querySelector(".footer");
+
+  console.dir(textEl.children[1]);
+
+  var showLines = new TimelineMax()
+    // 효두 로고 그리기
+    .add(drawHyodu, 0.15)
+    .addLabel("logo")
+    // 텍스트 등장
+    .to(textEl.children[0], 0.65, {
+      y: -15,
+      alpha: 1,
+      ease: Power1.easeOut
+    }, "logo+=1.75")
+    .to(textEl.children[1], 0.65, {
+      y: -15,
+      alpha: 1,
+      ease: Power1.easeOut
+    }, "logo+=1.95")
+    .addLabel("text")
+    //푸터 등장
+    .to(footerEl, 0.65, {
+      alpha: 1,
+    }, "text+=0.2");
+
+
+}());
