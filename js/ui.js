d6c266696ff43d49f4d756f622ee3760e1d75ad3
nana <nykim@nykim.net>
2019-11-26 10:58:17 +0900

    1차 작업 정리

--- js/ui.js
+++ js/ui.js
@@ -1,58 +1,84 @@
 (function () {
   var hyoduSVG = new Vivus("hyoduSVG", {
-    type: 'scenario-sync',
+    type: "scenario-sync",
     duration: 19,
-    start: 'manual',
+    start: "manual",
     forceRender: false,
     dashGap: 20
   });
 
   var lineSVG01 = new Vivus("lines", {
-    type: 'async',
+    type: "async",
     duration: 50,
-    start: 'autostart',
-    forceRender: false,
+    start: "autostart",
+    forceRender: false
   });
 
-
   var lineSVG02 = new Vivus("lines2", {
-    type: 'async',
+    type: "async",
     duration: 80,
-    start: 'autostart',
+    start: "autostart",
     forceRender: false,
     dashGap: 50
   });
 
-
   var drawHyodu = function () {
     hyoduSVG.reset().play();
-  }
+  };
 
+  var wrapEl = document.querySelector(".wrap");
   var textEl = document.querySelector(".aniText");
   var footerEl = document.querySelector(".footer");
-
-  console.dir(textEl.children[1]);
+  var hyoduShadowEl = document.querySelector("#hyoduSVG-shadow");
+  console.dir(wrapEl);
 
   var showLines = new TimelineMax()
+    .from(hyoduShadowEl, 0.9, {
+      alpha: 0.2
+    })
     // 효두 로고 그리기
     .add(drawHyodu, 0.15)
     .addLabel("logo")
     // 텍스트 등장
-    .to(textEl.children[0], 0.65, {
-      y: -15,
-      alpha: 1,
-      ease: Power1.easeOut
-    }, "logo+=1.75")
-    .to(textEl.children[1], 0.65, {
-      y: -15,
-      alpha: 1,
-      ease: Power1.easeOut
-    }, "logo+=1.95")
+    .to(
+      textEl.children[0],
+      0.75, {
+        y: -15,
+        alpha: 1,
+        ease: Power1.easeOut
+      },
+      "logo+=0.85"
+    )
+    .to(
+      textEl.children[1],
+      0.75, {
+        y: -15,
+        alpha: 1,
+        ease: Power1.easeOut
+      },
+      "logo+=1.05"
+    )
     .addLabel("text")
     //푸터 등장
-    .to(footerEl, 0.65, {
-      alpha: 1,
-    }, "text+=0.2");
+    .to(
+      footerEl,
+      0.65, {
+        alpha: 1
+      },
+      "text+=0.2"
+    );
 
 
-}());
+  var changeHeight = function () {
+    var winH = window.innerHeight;
+    wrapEl.style.height = winH + "px";
+  }
+
+  window.addEventListener("load", function () {
+    changeHeight();
+  });
+
+  window.addEventListener("resize", function () {
+    changeHeight();
+  });
+})();
